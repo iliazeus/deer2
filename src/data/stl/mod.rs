d04881a014c7f390008f15965ed6c37b6dfc1f90
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2022-08-23 00:48:07 +0300

    add stanford bunny STL model

--- src/data/stl/mod.rs
+++ src/data/stl/mod.rs
@@ -1,1 +1,8 @@
+/// Adapted from https://en.wikipedia.org/wiki/File:Utah_teapot_(solid).stl.
+/// Licensed under CC0 1.0
 pub const UTAH_TEAPOT: &[u8] = include_bytes!("utah_teapot.stl");
+
+
+// /// Adapted from https://commons.wikimedia.org/wiki/File:Stanford_Bunny.stl
+// /// Licensed under CC-BY 3.0
+// pub const STANFORD_BUNNY: &[u8] = include_bytes!("stanford_bunny.stl");
